7e61ade6014d30608563c9f37e5593b75935b93f
draftLuca <draftLuca@163.com>
2020-10-29 10:22:38 +0800

    feat:集合并发安全

---
+++ juc-thread-demo/src/main/java/review/thread/NotSafeDemo.java
@@ -0,0 +1,56 @@
+package review.thread;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * 并发控制
+ *  Collections.synchronized**()
+ *  juc对应的包
+ *
+ */
+public class NotSafeDemo {
+
+    public static void main(String[] args) {
+
+        //线程不安全
+/*        ArrayList<Object> list = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }).start();
+        }*/
+
+        //安全 效率低
+        /*List<Object> vector = new Vector<>();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                vector.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(vector);
+            }).start();
+        }*/
+
+        //使用Collections方法锁集合
+        List<Object> list2 = Collections.synchronizedList(new ArrayList<>());
+        /*for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                list2.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list2);
+            }).start();
+        }*/
+
+        //使用juc 包的CopyOnWriteArrayList
+ /*       List<Object> list3 =new CopyOnWriteArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                list3.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list3);
+            }).start();
+        }*/
+
+
+    }
+
+}
